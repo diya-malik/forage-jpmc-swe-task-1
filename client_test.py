b309aa00f2a9f4d46165629c92fee7e03e9130c0
dmlk131 <diya2003mlk@gmail.com>
2023-05-30 16:23:34 +0530

    Task_1
    Completed

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -7,6 +8,7 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+    self.assertEqual(getDataPoint(quotes),{quotes['stock'],quotes['top_bid']['price'], quotes['top_ask']['price'],(quotes['top_bid']['price']+quotes['top_ask']['price'])/2})
     """ ------------ Add the assertion below ------------ """
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
@@ -14,9 +16,27 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+    self.assertEqual(getDataPoint(quotes),{quotes['stock'],quotes['top_bid']['price'], quotes['top_ask']['price'],(quotes['top_bid']['price']+quotes['top_ask']['price'])/2})
     """ ------------ Add the assertion below ------------ """
 
-
+  def test_getRatio_biszero(self):
+     quotes = [
+        {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+        {'top_ask': {'price': 0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+     ]
+     self.assertIsNone(getRatio(
+        ((quotes[0]['top_bid']['price']+quotes[0]['top_ask']['price'])/2)/((quotes[1]['top_bid']['price']+quotes[1]['top_ask']['price'])/2)
+     )
+    )
+     
+  def test_getRatio_aiszero(self):
+     quotes = [
+        {'top_ask': {'price': 0, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+        {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+     ]
+     self.assertEqual(getRatio(
+        ((quotes[0]['top_bid']['price']+quotes[0]['top_ask']['price'])/2)/((quotes[1]['top_bid']['price']+quotes[1]['top_ask']['price'])/2)
+     ),0)
   """ ------------ Add more unit tests ------------ """
 
 
